f315260d721bb50ef45492eb7b510b26d74ac0a1
jheferlinares <linaresjhefer@gmail.com>
2024-10-04 21:19:09 -0400

    aver

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ const expressLayouts = require("express-ejs-layouts")
 const env = require("dotenv").config()
 const app = express()
 const static = require("./routes/static")
+const baseController = require("./controllers/baseController")
 
 // View Engine and Templates
 app.set("view engine", "ejs")
@@ -21,9 +22,7 @@ app.set("layout", "./layouts/layout") // not at views root
  *************************/
 app.use(static)
 //Index route
-app.get("/", function(req, res){
-  res.render("index", {tittle: ""})
-})
+app.get("/", baseController.buildHome)
 
 /* ***********************
  * Local Server Information
